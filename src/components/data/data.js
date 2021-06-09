2899482de130e2275b908242f99c9988670201eb
Shadow129-sys <gsayankr02@gmail.com>
2021-06-10 01:51:06 +0530

    update 3

--- src/components/data/data.js
+++ src/components/data/data.js
@@ -4,7 +4,7 @@ const data = [
 		body: "Originally unveiled in 2007 for the iPhone, iOS has been extended to support other Apple devices such as the iPad Touch (September 2007) and the iPad (January 2010).As of March 2018, Apple's App Store contains more than 2.1 million of which ar native for iPads.These mobile apps have collectively been downloaded more than 130 billion times.",
 	},
 	{
-		head: "Earbuds",
+		head: "Earpods",
 		body: "Unlike traditional, circular earbuds, the design of the EarPods is defined by the geometry of the ear. Which makes them more comfortable for more people than any other earbud-style headphones. The speakers inside the EarPods have been engineered to maximize sound output and minimize sound loss, which means you get high-quality audio.",
 	},
 	{
